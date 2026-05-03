a58b7a656309a0bc02396f07978fc8d966907bbf
Manu-REMY <emmanuel.remy@ac-montpellier.fr>
2026-05-03 10:25:41 +0200

    lang: add Phase 3 CDCF provided/hybrid mode strings

--- gestionprojet/version.php
+++ gestionprojet/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gestionprojet';
-$plugin->version = 2026050301;  // YYYYMMDDXX format
+$plugin->version = 2026050302;  // YYYYMMDDXX format
 $plugin->requires = 2024100700; // Moodle 5.0+
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '2.2.0-dev (phase 3)';
